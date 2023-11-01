b5c9d3548fe87a40b90fe1676165cec54322a7c2
tylerkennedy <kennedytyler25@icloud.com>
2023-11-01 18:55:26 -0400

    Create embeddings

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ from html.parser import HTMLParser
 from urllib.parse import urlparse
 import os
 
+import pandas as pd
+import tiktoken
+import openai
+
 # Regex pattern to match a URL
 HTTP_URL_PATTERN = r'^http[s]*://.+'
 
 domain = "donttext.app" # <- put your domain to be crawled
 full_url = "https://donttext.app/" # <- put your domain to be crawled with https or http
+
+max_tokens = 500
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Create a class to parse the HTML and get the hyperlinks
 class HyperlinkParser(HTMLParser):
@@ -132,9 +139,107 @@ def crawl(url):
                 queue.append(link)
                 seen.add(link)
 
+def remove_newlines(serie):
+    serie = serie.str.replace('\n', ' ')
+    serie = serie.str.replace('\\n', ' ')
+    serie = serie.str.replace('  ', ' ')
+    serie = serie.str.replace('  ', ' ')
+    return serie
+
+# Function to split the text into chunks of a maximum number of tokens
+def split_into_many(text, tokenizer, max_tokens = max_tokens):
+
+    # Split the text into sentences
+    sentences = text.split('. ')
+
+    # Get the number of tokens for each sentence
+    n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
+
+    chunks = []
+    tokens_so_far = 0
+    chunk = []
+
+    # Loop through the sentences and tokens joined together in a tuple
+    for sentence, token in zip(sentences, n_tokens):
+
+        # If the number of tokens so far plus the number of tokens in the current sentence is greater
+        # than the max number of tokens, then add the chunk to the list of chunks and reset
+        # the chunk and tokens so far
+        if tokens_so_far + token > max_tokens:
+            chunks.append(". ".join(chunk) + ".")
+            chunk = []
+            tokens_so_far = 0
+
+        # If the number of tokens in the current sentence is greater than the max number of
+        # tokens, go to the next sentence
+        if token > max_tokens:
+            continue
+
+        # Otherwise, add the sentence to the chunk and add the number of tokens to the total
+        chunk.append(sentence)
+        tokens_so_far += token + 1
+
+    return chunks
+
+def create_embeddings():
+    # Create a list to store the text files
+    texts=[]
+
+    # Get all the text files in the text directory
+    for file in os.listdir("text/" + domain + "/"):
+
+        # Open the file and read the text
+        with open("text/" + domain + "/" + file, "r", encoding="UTF-8") as f:
+            text = f.read()
+
+            # Omit the first 13 characters and the last 4 characters of the file name, then replace -, _, and #update with spaces.
+            texts.append((file[13:-4].replace('-',' ').replace('_', ' ').replace('#update',''), text)) # Add filename and content as a tuple to texts
+
+    # Create a dataframe from the list of texts
+    df = pd.DataFrame(texts, columns = ['fname', 'text'])
+
+    # Set the text column to be the raw text with the newlines removed
+    df['text'] = df.fname + ". " + remove_newlines(df.text)
+    df.to_csv('processed/scraped.csv')
+    df.head()
+
+    # Load the cl100k_base tokenizer which is designed to work with the ada-002 model
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+
+    df = pd.read_csv('processed/scraped.csv', index_col=0)
+    df.columns = ['title', 'text']
+
+    # Tokenize the text and save the number of tokens to a new column
+    df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+
+    shortened = []
+
+    # Loop through the dataframe
+    for row in df.iterrows():
+
+        # If the text is None, go to the next row
+        if row[1]['text'] is None:
+            continue
+
+        # If the number of tokens is greater than the max number of tokens, split the text into chunks
+        if row[1]['n_tokens'] > max_tokens:
+            shortened += split_into_many(row[1]['text'], tokenizer)
+
+        # Otherwise, add the text to the list of shortened texts
+        else:
+            shortened.append( row[1]['text'] )
+
+    df = pd.DataFrame(shortened, columns = ['text'])
+    df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+
+    df['embeddings'] = df.text.apply(lambda x: openai.Embedding.create(input=x, engine='text-embedding-ada-002')['data'][0]['embedding'])
+
+    df.to_csv('processed/embeddings.csv')
+    df.head()
 
 def main():
     crawl(full_url)
+    create_embeddings()
 
 if __name__ == "__main__":
     main()
